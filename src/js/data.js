3e254c33156421079bafda682cb3af265993985f
readytoball <66325180+readytoball@users.noreply.github.com>
2020-06-02 18:37:47 -0400

    test

--- src/js/data.js
+++ src/js/data.js
@@ -15,7 +15,7 @@ const dataSet = {};
  * 
  * @example '2018-02-20'
 */
-let dataSetVersion = '';
+let dataSetVersion = '2020-06-02';
 
 /**
  * Image root, will be appended to the start of every image URL.
