6c0c139c29c9efab65d25b627b7e15eba5608939
readytoball <66325180+readytoball@users.noreply.github.com>
2020-06-02 19:42:42 -0400

    Update 2019-11-26.js

--- src/js/data/2019-11-26.js
+++ src/js/data/2019-11-26.js
@@ -49,6 +49,256 @@ dataSet[dataSetVersion].characterData = [
     }
   },
   {
+    name: "Setting Off",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "Provincial City of Rolent",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"}},
+  {
+    name: "Rock on the Road",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Sophisticated Fight",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: [ "skyfc"]}
+  },
+  {
+    name: "Defeat",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Tetracyclic Tower",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"]}
+  },
+  {
+    name: "Under the Moonlight",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "A Cat Relaxing in the Sun",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: [ "skyfc"],
+    }
+  },
+  {
+    name: "Secret Green Passage",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "In My Heart",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "The Way They Walk in Liberl",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Commercial City of Bose",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "Border Patrol Isn't Easy",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "Steel Floor Blocking the Way",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "We Are the Capua Family!",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "To Be Suggestive",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"]	}
+  },
+  {
+    name: "Pinch",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Disappearing Star",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"]
+    }
+  },
+  {
+    name: "Seaport City of Ruan",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: [ "skyfc"],
+    }
+  },
+  {
+    name: "Don't Let Him Go!",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "Pride of Liberl",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "Creeping Crisis",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "Wandering in the Darkness",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Factory City of Zeiss",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Peace Bestowed by Twilight",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Leiston Fortress",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Engaged People in Secret Maneuvers",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "Royal Capital of Grancel",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Grand Arena",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Challenger Invited",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Royal Castle",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "Recapture",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"}
+  },
+  {
+    name: "Silver Will",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"}
+  },
+  {
+    name: "Hollow Light of the Sealed Land",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"],
+    }
+  },
+  {
+    name: "Ancient Makes",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"]	}
+  },
+  {
+    name: "Those Who Protect the Greatest Treasure",
+    img: "rbFKdHE.jpg",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Go in Good Cheer",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Release from the Spell, and...",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Confession",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Decision to Leave",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Hoshi no Arika",
+    img: "rbFKdHE.jpg?1",
+    opts: {
+      series: ["skyfc"]
+    }
+  },
+  {
+    name: "Feelings Soar with the Wind",
+    img: "rbFKdHE.jpg?1",
+    opts: { series: ["skyfc"] }
+  },
+  {
+    name: "Welcome to Le-Locle",
+    img: "3ekPRF1.jpg?1",
+    opts: { series: ["skysc"] }
+  },
+  {
+    name: "Strepitoso Fight",
+    img: "3ekPRF1.jpg?1",
+    opts: { series: ["skysc"] }
+  },
+  {
     name: "Kirisame Marisa",
     img: "tJnkSzK.png",
     opts: {
