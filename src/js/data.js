ec9e62497b17fa65744880e7ab6f04eec96fbf12
readytoball <66325180+readytoball@users.noreply.github.com>
2020-06-02 20:58:46 -0400

    Update data.js

--- src/js/data.js
+++ src/js/data.js
@@ -15,7 +15,7 @@ const dataSet = {};
  * 
  * @example '2018-02-20'
 */
-let dataSetVersion = '2020-06-02';
+let dataSetVersion = '';
 
 /**
  * Image root, will be appended to the start of every image URL.
